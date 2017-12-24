f71e26cb60b76badec3057c47ed95dc8431d160e
chanducky <chandu.cky@gmail.com>
2017-12-24 16:48:11 +0530

    added new commponent to add question ans form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { QuestionListComponent } from './components/question-list/question-list.component';
 import { DataService } from "../app/services/data.service";
 import { QuestionComponent } from './components/question/question.component';
+import { AddQuestionComponent } from './components/add-question/add-question.component';
 
 
 @NgModule({
@@ -13,10 +15,11 @@ import { QuestionComponent } from './components/question/question.component';
     AppComponent,
     NavbarComponent,
     QuestionListComponent,
-    QuestionComponent
+    QuestionComponent,
+    AddQuestionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule
   ],
   providers: [
     DataService
