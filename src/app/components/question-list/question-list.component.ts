d0ab3d93300cf1ea5a4178c654796e44ac26c537
chanducky <chandu.cky@gmail.com>
2017-12-24 16:08:36 +0530

    added nested components

---
+++ src/app/components/question-list/question-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Question } from "../../models/Question";
+import { DataService} from "../../services/data.service";
+
+@Component({
+  selector: 'app-question-list',
+  templateUrl: './question-list.component.html',
+  styleUrls: ['./question-list.component.css']
+})
+export class QuestionListComponent implements OnInit {
+
+  questions:Question[];
+  constructor(private dataService:DataService) { 
+   
+  }
+
+  ngOnInit() {
+    //console.log(this.dataService.getQuestions());
+    this.questions=this.dataService.getQuestions();
+  }
+
+}
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Question} from "../models/Question";
+
+@Injectable()
+export class DataService {
+  questions:Question[];
+  constructor() { 
+    this.questions=[
+      {
+        text:"What is your name..?",
+        answer:"Chandu Yadav",
+        hide:true
+      },
+      {
+        text:"What is faveroite color..?",
+        answer:"white",
+        hide:true
+      },
+      {
+        text:"What is your language?",
+        answer:"Java",
+        hide:true
+      }
+    ];
+  }
+
+  getQuestions(){
+    return this.questions;
+  }
+
+
+}
