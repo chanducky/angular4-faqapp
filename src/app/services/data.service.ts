b0734d4b4e3800e3ea4d1702d67311094fe8fa2d
chanducky <chandu.cky@gmail.com>
2017-12-24 17:55:44 +0530

    finish

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -25,18 +25,32 @@ export class DataService {
   }
 
   getQuestions(){
+    if(localStorage.getItem('questions') !==null){
+      this.questions=JSON.parse(localStorage.getItem('questions'));
+    }else{
+      this.questions=[];
+    }
+    
     return this.questions;
   }
 
   addQuestion(question:Question){
     this.questions.unshift(question);
+    let questionsLS=[];
+    if(localStorage.getItem('questions') !=null){
+      questionsLS = JSON.parse(localStorage.getItem('questions')); 
+    }
+    questionsLS.unshift(question);
+    localStorage.setItem('questions',JSON.stringify(questionsLS));
+    
   }
   
   removeQuestion(question:Question){
-    console.log('in remove '+question);
+    
     for(let i=0;i<this.questions.length;i++){
       if(this.questions[i] == question){
         this.questions.splice(i,1);
+        localStorage.setItem('questions',JSON.stringify(this.questions));
       }
     }
   }
